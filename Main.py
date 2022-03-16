2f2186802575e4e369ff1e1ba561c62eada52fd9
Julius Khawjazadeh <juliuskhawjazadeh@Juliuss-MacBook-Pro.local>
2022-03-16 06:44:34 -0400

    Added the score and sound effects

--- Main.py
+++ Main.py
@@ -1,6 +1,8 @@
 import pygame
 import random
 
+pygame.init()
+
 WIDTH, HEIGHT = 700, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
@@ -9,8 +11,13 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 FPS = 60
 
-ballVel = 3
+ballVel = 6
 starting_dir_list = [-ballVel, ballVel]
+
+font = pygame.font.Font("Assets/Fonts/BigStarRegular.ttf", 110)
+collision_sound = pygame.mixer.Sound("Assets/Sound_Effects/paddle_collision.wav")
+
+
 
 class PongEntity:
     def __init__(self, x, y, width, height):
@@ -58,10 +65,17 @@ class Ball (PongEntity):
 
 class Paddle (PongEntity):
     def getPaddleTop(self):
-        return pygame.Rect((self.x, self.y), (self.width, self.height/2))
+        return pygame.Rect((self.x, self.y), (self.width, self.height/3))
+
+    def getEntirePaddle(self):
+        return pygame.Rect((self.x, self.y), (self.width, self.height))
 
     def getPaddleBottom(self):
-        return pygame.Rect((self.x, self.y + self.height/2), (self.width, self.height/2))
+        return pygame.Rect((self.x, (self.y + self.height) - self.height/3), (self.width, self.height/3))
+
+    def getPaddleMiddle(self):
+        return pygame.Rect((self.x, self.y + self.height/3), (self.width, self.height/3))
+
 
 class Divider:
     def __init__(self, x, width, height):
@@ -81,17 +95,17 @@ class Divider:
           pygame.draw.rect(WIN, WHITE, (self.x, y, self.width, self.height))
     
 
-P1_SIZE_X, P1_SIZE_Y = 8, 110
-B_SIZE = 10
+P_SIZE_X, P_SIZE_Y = 14, 80
+B_SIZE = 15
 D_SIZE_X = 4
 D_SIZE_Y = 80
 
 # Paddles
 
-p1 = Paddle(P1_SIZE_X, HEIGHT/2 - P1_SIZE_Y/2, P1_SIZE_X, P1_SIZE_Y)
-p2 = Paddle((WIDTH - P1_SIZE_X) - P1_SIZE_X, HEIGHT/2 - P1_SIZE_Y/2, P1_SIZE_X, P1_SIZE_Y)
+p1 = Paddle(P_SIZE_X/2, HEIGHT/2 - P_SIZE_Y/2, P_SIZE_X, P_SIZE_Y)
+p2 = Paddle((WIDTH - P_SIZE_X/2) - P_SIZE_X, HEIGHT/2 - P_SIZE_Y/2, P_SIZE_X, P_SIZE_Y)
 
-DIVIDER_WIDTH, DIVIDER_HEIGHT = 4, 20
+DIVIDER_WIDTH, DIVIDER_HEIGHT = 4, 5
 
 # BALL
 ball_reset_posX = WIDTH/2 - B_SIZE/2
@@ -124,11 +138,14 @@ def reset_ball(b):
     
 
 def main():
+    player1_score = 0
+    player2_score = 0
     run = True
     paddle_vel = 8
     clock = pygame.time.Clock()
     dirX = starting_dir_list[random.randrange(0, len(starting_dir_list))] # randomizing whoever gets the ball first
     dirY = starting_dir_list[random.randrange(0, len(starting_dir_list))] # randomizing whoever gets the ball first
+    temp = dirY+1
 
     while run:
         clock.tick(FPS)
@@ -147,7 +164,10 @@ def main():
         if keys_pressed[pygame.K_UP]:
             p2.moveY(-paddle_vel)
         elif keys_pressed[pygame.K_DOWN]:
-            p2.moveY(paddle_vel)
+            p2.moveY(paddle_vel) 
+
+        # UNCOMMENT TO ACTIVATE SINGLE PLAYER MODER
+        #p2.moveY(dirY-1)
 
         # BALL PHYSICS
         b.move(dirX, dirY)
@@ -157,55 +177,87 @@ def main():
         elif b.getY() + b.getHeight() > HEIGHT:
             dirY *= -1
 
-        if p1.getPaddleTop().colliderect(b.getRect()) and dirY > 0:
-            print("TOP going down")
+        if p1.getPaddleTop().colliderect(b.getRect()) and dirY >= 0 :
+            print("TOP")
+            dirY = temp
             dirY *= -1
             dirX *= -1
-        elif p1.getPaddleTop().colliderect(b.getRect()) and dirY < 0:
-            print("TOP going up")
+        elif p1.getPaddleTop().colliderect(b.getRect()) and dirY <= 0:
+            print("TOP")
+            dirY = temp
             dirX *= -1
-        elif p1.getPaddleBottom().colliderect(b.getRect()) and dirY > 0:
-            print("Bottom going down")
+        elif p1.getPaddleBottom().colliderect(b.getRect()) and dirY >= 0:
+            print("BOTTOM")
+            dirY = temp
             dirX *= -1
-        elif p1.getPaddleBottom().colliderect(b.getRect()) and dirY < 0:
-            print("Bottom going up")
+        elif p1.getPaddleBottom().colliderect(b.getRect()) and dirY <= 0:
+            print("BOTTOM")
+            dirY = temp 
             dirY *= -1
+            dirX *= -1
+        elif p1.getPaddleMiddle().colliderect(b.getRect()):
+            print("MIDDLE")
+            dirY *= 0
             dirX *= -1
         
-        if p2.getPaddleTop().colliderect(b.getRect()) and dirY > 0:
-            print("TOP going down")
+        if p2.getPaddleTop().colliderect(b.getRect()) and dirY >= 0:
+            print("TOP")
+            dirY = temp
             dirY *= -1
             dirX *= -1
-        elif p2.getPaddleTop().colliderect(b.getRect()) and dirY < 0:
-            print("TOP going up")
+        elif p2.getPaddleTop().colliderect(b.getRect()) and dirY <= 0:
+            print("TOP")
+            dirY = temp
             dirX *= -1
-        elif p2.getPaddleBottom().colliderect(b.getRect()) and dirY > 0:
-            print("Bottom going down")
+        elif p2.getPaddleBottom().colliderect(b.getRect()) and dirY >= 0:
+            print("BOTTOM")
+            dirY = temp
             dirX *= -1
-        elif p2.getPaddleBottom().colliderect(b.getRect()) and dirY < 0:
-            print("Bottom going up")
+        elif p2.getPaddleBottom().colliderect(b.getRect()) and dirY <= 0:
+            print("BOTTOM")
+            dirY = temp 
             dirY *= -1
             dirX *= -1
+        elif p2.getPaddleMiddle().colliderect(b.getRect()):
+            print("MIDDLE")
+            dirY *= 0
+            dirX *= -1
+
+        # Paddle collision sound effect
+        if p1.getEntirePaddle().colliderect(b.getRect()) or p2.getEntirePaddle().colliderect(b.getRect()):
+            collision_sound.play()
         
 
         if b.getX() < 0:
-            # player one wins a point
+            player2_score += 1
             reset_ball(b)
             dirY *= -1
             dirX *= -1
         elif b.getX() + b.getWidth() > WIDTH:
-            # player two wins a point
+            player1_score += 1
             reset_ball(b)
             dirY *= -1
             dirX *= -1
 
 
+        player1_text = font.render(str(player1_score), True, WHITE)
+        player2_text = font.render(str(player2_score), True, WHITE)
+
+        player1_text_rect = player1_text.get_rect()
+        player2_text_rect = player2_text.get_rect()
+
+        player1_text_rect.center = ((WIDTH/2) / 2, player1_text_rect.height/2)
+        player2_text_rect.center = ((WIDTH/2) + ((WIDTH/2) / 2), player2_text_rect.height/2)
+
         
+        print(dirY)
 
         setBoundries(p1, p2)        
 
         
         WIN.fill((BLACK))
+        WIN.blit(player1_text, player1_text_rect)
+        WIN.blit(player2_text, player2_text_rect)
         draw_window()
 
     pygame.quit()
